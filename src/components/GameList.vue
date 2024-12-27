b73df04fb5fae1be752977987861a02d3644902f
Gerardo Macias <correojerard@gmail.com>
2024-12-27 21:27:28 +0100

    little refactors

--- src/components/GameList.vue
+++ src/components/GameList.vue
@@ -8,7 +8,7 @@
     <div v-if="randomGames.length" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
       <div v-for="(game) in randomGames" :key="game.id" class="bg-gray-800 shadow-md rounded-lg p-4" role="article">
         <!-- Imagen y enlace al juego -->
-        <a :href="`/game?id=${game.id}`" class="block h-32 rounded-md mb-4" aria-label="Link to {{ game.title }}">
+        <a :href="`/game?id=${game.id}`" class="block h-52 lg:h-32 rounded-md mb-4" aria-label="Link to {{ game.title }}">
           <img :src="game.thumbnail" :alt="'Thumbnail of ' + game.title" class="w-full h-full object-cover rounded-md" />
         </a>
         <!-- Categoría del juego -->
